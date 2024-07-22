7b4952824fc35c827f7cbb05968d3ece21b782c0
Luka Stevanovic <107753498+LukaaStevanovic@users.noreply.github.com>
2024-07-22 18:44:25 +0200

    Funkcije domaci

---
+++ funkcije/index.php
@@ -0,0 +1,17 @@
+<?php
+
+    function izracunaj_pdv($cena)
+    {
+        if(is_string($cena)){
+            die("Broj mora biti numericka vrednost");
+        }else{
+            if($cena < 1){
+                echo "Broj ne moze biti manji od 1";
+            }else
+            {
+                $cena_pdv = $cena * 0.22;
+            }
+        }
+        echo "PDV iznosi $cena_pdv";
+    }
+    izracunaj_pdv("steva");
